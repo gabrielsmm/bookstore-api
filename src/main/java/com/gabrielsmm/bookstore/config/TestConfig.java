0ac577ac0970d54ad279b5927cab9dc5a451eaa5
Gabriel Mendes <gabrielsmm251@gmail.com>
2021-08-01 11:40:01 -0300

    Perfil de teste implementado

--- src/main/java/com/gabrielsmm/bookstore/config/TestConfig.java
+++ src/main/java/com/gabrielsmm/bookstore/config/TestConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -11,27 +12,18 @@ import com.gabrielsmm.bookstore.entities.Categoria;
 import com.gabrielsmm.bookstore.entities.Livro;
 import com.gabrielsmm.bookstore.repositories.CategoriaRepository;
 import com.gabrielsmm.bookstore.repositories.LivroRepository;
+import com.gabrielsmm.bookstore.services.DBService;
 
 
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner {
+public class TestConfig {
 	
 	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
-	@Autowired
-	private LivroRepository livroRepository;
+	private DBService dbService;
 
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean Code","Robert Martin", "Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
+	@Bean
+	public void instanciaBaseDeDados() {
+		this.dbService.instanciaBaseDeDados();
 	}
 }
